d70d2e5ebb01cbcc921c23a4dc08e1c91089791d
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2022-09-02 15:55:14 +0200

    added nested switch statement.

--- converter.js
+++ converter.js
@@ -1,48 +1,59 @@
 function convertToRoman(num){
 
     const numStr = num.toString();
+    console.log(numStr);
 
-    console.log(num);
+    const strLength = numStr.length;
+    console.log(strLength);
 
-    switch(num){
-        case 1000:
+
+
+    switch(strLength){
+        case 4:
             return 'M';
             break;
-        case 900:
+        case 3:
             return 'CM';
             break;
-        case 500:
+        case 2:
             return 'D';
             break;
-        case 400:
-            return 'CD';
-            break;
-        case 90:
-            return 'XC';
-        case 50:
-            return 'L';
-            break;
-        case 40:
-            return 'XL';
-            break;
-        case 10:
-            return 'X';
-            break;
-        case 9:
-            return 'IX';
-            break;
-        case 5:
-            return 'V';
-            break;
-        case 4:
-            return 'IV';
-            break;
         case 1:
-            return 'I';
+            switch(numStr){
+                case '1':
+                    console.log('I');
+                    break;
+                case '2':
+                    console.log('II');
+                    break;
+                case '3':
+                    console.log('III');
+                    break;
+                case '4':
+                    console.log('IV');
+                    break;
+                case '5':
+                    console.log('V');
+                    break;
+                case '6':
+                    console.log('VI');
+                    break;
+                case '7':
+                    console.log('VII');
+                    break;
+                case '8':
+                    console.log('VIII');
+                    break;
+                case '9':
+                    console.log('IX');
+                    break;
+                default:
+                    console.log('Invalid character.');
+            }
             break;
         default:
-            return 'Invalid numeral.';      
+            return 'Empty String.';      
     }
 }
 
-convertToRoman(1000);
+convertToRoman(10);
