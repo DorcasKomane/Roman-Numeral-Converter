8966e4000d6e1c3c7c2f709e3baddb9a63dc9f26
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2022-09-07 15:19:02 +0200

    added objects that hold the roman number values.

--- converter.js
+++ converter.js
@@ -1,59 +1,66 @@
 function convertToRoman(num){
 
-    const numStr = num.toString();
-    console.log(numStr);
+    const ones = {
+        0: '0',
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        6: 'VI',
+        7: 'VII',
+        8: 'VIII',
+        9: 'IX',
+    };
 
-    const strLength = numStr.length;
-    console.log(strLength);
+    const tens = {
+        10: 'X',
+        11: 'XI',
+        12: 'XII',
+        13: 'XIII',
+        14: 'XIV',
+        15: 'XV',
+        16: 'XVI',
+        17: 'XVII',
+        18: 'XVIII',
+        19: 'XIX'
+    };
 
+    const hundreds = {
+        100: 'C',
+        200: 'CC',
+        300: 'CCC',
+        400: 'CD',
+        500: 'D',
+        600: 'DC',
+        700: 'DCC',
+        800: 'DCCC',
+        900: 'CM',
+    };
 
+    const thousands = {
+        1000: 'M'
+    };
 
-    switch(strLength){
-        case 4:
-            return 'M';
-            break;
-        case 3:
-            return 'CM';
-            break;
-        case 2:
-            return 'D';
-            break;
-        case 1:
-            switch(numStr){
-                case '1':
-                    console.log('I');
-                    break;
-                case '2':
-                    console.log('II');
-                    break;
-                case '3':
-                    console.log('III');
-                    break;
-                case '4':
-                    console.log('IV');
-                    break;
-                case '5':
-                    console.log('V');
-                    break;
-                case '6':
-                    console.log('VI');
-                    break;
-                case '7':
-                    console.log('VII');
-                    break;
-                case '8':
-                    console.log('VIII');
-                    break;
-                case '9':
-                    console.log('IX');
-                    break;
-                default:
-                    console.log('Invalid character.');
-            }
-            break;
-        default:
-            return 'Empty String.';      
+    const prefixes = {
+        2: 'XX',
+        3: 'XXX',
+        4: 'XL',
+        5: 'L',
+        6: 'LX',
+        7: 'LXX',
+        8: 'LXXX',
+        9: 'XC'
+    };
+
+    let romanNum = 'ZERO';
+
+    if(num in ones){
+        romanNum = ones[num];
     }
+
+    //return romanNum;
+    console.log(romanNum);
 }
 
-convertToRoman(10);
+convertToRoman(1);
